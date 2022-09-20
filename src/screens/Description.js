d37d46944597130b4b202760b6512c0f1dfa00e0
Kevin Guzman <guzmankevin025@gmail.com>
2022-09-20 10:56:21 -0600

    Home button added on Description screen, and data (A) finished

--- src/screens/Description.js
+++ src/screens/Description.js
@@ -1,41 +1,59 @@
-import { StyleSheet, Text, ScrollView, ImageBackground } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  ImageBackground,
+ 
+} from "react-native";
+import React from "react";
+import { IconButton } from "react-native-paper";
 
-const Description = ( {route, navigation}) => {
+const Description = ({ route, navigation }) => {
+  const image = require("../../assets/background05.jpg");
 
-  const image =require("../../assets/background05.jpg");
+  const { descript, name } = route.params;
 
-  const {descript, name} = route.params
+  navigation.setOptions({
+    title: name,
+    headerRight: () => (
+    
+        <IconButton
+          icon="home"
+          iconColor={"#fff"}
+          size={23}
+          onPress={() => navigation.navigate("HomePage")}
+        />
+     
+    ),
+  });
 
-  navigation.setOptions({ title: name })
-  
   return (
-    <ImageBackground source={ image } resizeMode="cover"  style={{width: '100%', height: '100%'}}>
-    <ScrollView  showsVerticalScrollIndicator={false} style={styles.container}>
-   
-      <Text style={styles.text}>{descript}</Text>
-    </ScrollView>
+    <ImageBackground
+      source={image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+        <Text style={styles.text}>{descript}</Text>
+      </ScrollView>
     </ImageBackground>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     paddingHorizontal: 15,
     paddingVertical: 15,
     color: "#000",
-
   },
-  text:{
-   
+  text: {
     fontSize: 20,
-    padding:10,
+    padding: 10,
     lineHeight: 30,
     fontFamily: "lucida grande",
-    color:"#000",
-  }
-})
-
+    color: "#000",
+  },
+});
